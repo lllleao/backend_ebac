b7bced6c94184514d51527cd32c0888900c739e8
leao <lucasleaolima@gmail.com>
2025-06-21 19:54:53 -0300

    feat autodeploy

--- backend/urls.py
+++ backend/urls.py
@@ -19,9 +19,11 @@ from django.urls import include, path
 
 from django.conf.urls.static import static
 from django.conf import settings
+from backend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('posts.urls')),
+    path('update_server/', views.update, name="update"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
