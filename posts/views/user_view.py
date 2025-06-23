6f96809f6af9591cda1cd9eff7e3f0ac336ede97
leao <lucasleaolima@gmail.com>
2025-06-22 22:20:43 -0300

    view authenticated

--- posts/views/user_view.py
+++ posts/views/user_view.py
@@ -1,9 +1,13 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from ..models import Usuario
 from ..serializers import UsuarioSignupSerializer
 
 class UserView(ModelViewSet):
     serializer_class = UsuarioSignupSerializer
+    queryset = Usuario.objects.all().order_by("id")
 
-    def get_queryset(self):
-        return Usuario.objects.all().order_by("id")
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [AllowAny()]
+        return [IsAuthenticated()]
